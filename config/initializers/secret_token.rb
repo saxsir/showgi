2b89bd9b41f5835f6a4b033524c34091131dded7
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2012-12-31 17:16:11 +0900

    create new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showgi::Application.config.secret_token = 'a21b00353ecf94e66e0db5735485bf57020c91641dce92487afb90c27939b8382a17daac5f64f5568922660298c5d5671e7afa8f7f0721f21b1ad70af0b6ad69'
