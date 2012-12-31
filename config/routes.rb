ff632d39915a276d5b92dca3be95d2084680e32d
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2012-12-31 18:35:22 +0900

    home#indexをroot_pathに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Showgi::Application.routes.draw do
-
-  get "home/index"
-
+  
+  root :to => 'home#index'
+	
   devise_for :users
 
-  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
