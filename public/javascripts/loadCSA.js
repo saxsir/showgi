88d52329778596c0de40db1f0f2e41060eafb7d6
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-01-02 11:17:23 +0900

    CSAファイルの読み込みできたよ

---
+++ public/javascripts/loadCSA.js
@@ -0,0 +1,14 @@
+var loadCSA = function(filePath) {
+    httpObj = new XMLHttpRequest();
+    // httpObj.open('GET', filePath, true);
+    httpObj.open('GET', filePath + "?" +(new Date()).getTime(), true);
+    // ?以降はキャッシュされたファイルではなく、毎回読み込むためのもの
+    httpObj.send(null);
+    httpObj.onreadystatechange = function(){
+	if ( (httpObj.readyState == 4) && (httpObj.status == 200) ){
+            document.getElementById("csa").innerHTML = httpObj.responseText;
+	    makeBoard("board", "csa");
+	}
+    }
+}
+
