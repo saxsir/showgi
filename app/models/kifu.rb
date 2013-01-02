65d43480d9f3fbf1315b5ab18f1c423ea5321cd7
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-01-02 11:46:18 +0900

    ユーザーと棋譜データの関連付け

--- app/models/kifu.rb
+++ app/models/kifu.rb
@@ -1,3 +1,13 @@
 class Kifu < ActiveRecord::Base
   attr_accessible :file_url, :owner_id, :title
+  
+  belongs_to :user
+
+  def upload
+    file=params[:file]['csadata']
+    @filename = params[:file]['csadata'].original_filename
+    File.open("public/upload/#{@filename}", "wb"){ |f|
+      f.write(file.read)
+    }
+  end
 end
