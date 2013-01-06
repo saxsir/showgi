8c774cd653982b832d55327cff3fa2a078f3d984
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-01-06 23:32:26 +0900

    コード整形

--- app/controllers/userpage_controller.rb
+++ app/controllers/userpage_controller.rb
@@ -3,7 +3,7 @@ class UserpageController < ApplicationController
   # GET /userpage
   def index
     @favorites = Favorite.find(:all, :conditions => ['user_id = ?', current_user.id])
-    
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @kifus }
